1b163dbf2a29592f8e7d5fa71fb6cc16a7f23ae0
Ali Chapman <18708388+Alitech3@users.noreply.github.com>
2024-10-05 19:40:38 -0400

    Link to Game Test- REMOVE IT

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,49 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="h-screen bg-center bg-gradient-to-tr from-green-700 to-green-400">
+    return (
+        <main className="h-screen bg-center bg-gradient-to-tr from-green-700 to-green-400">
+            <div>
+                <Link href={"./game"}>Game - REMOVE ME</Link>
+            </div>
+            <div className="flex flex-col items-center justify-start h-screen bg-repeat bg-[length:106px] bg-bomb-pattern">
 
-        <div className="flex flex-col items-center justify-start h-screen bg-repeat bg-[length:106px] bg-bomb-pattern">
-
-          <div className = "flex flex-col justify-center items-center bg-center avatar aspect-square overflow-visible h-screen bg-explosion bg-no-repeat bg-[length:950px]" >
+                <div className = "flex flex-col justify-center items-center bg-center avatar aspect-square overflow-visible h-screen bg-explosion bg-no-repeat bg-[length:950px]" >
             
-              <div className = "flex flex-col justify-center items-center mt-10 mb-28">
+                    <div className = "flex flex-col justify-center items-center mt-10 mb-28">
                   
-                  <h1 className="text-6xl font-bold tracking-wide my-12">
+                        <h1 className="text-6xl font-bold tracking-wide my-12">
                     Bomber Chess
-                  </h1>
+                        </h1>
                     
-                </div>
+                    </div>
                   
-                <div className="flex flex-col justify-center space-y-12"> 
-                  <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
-                      <Link className = "text-xl font-bold tracking-widest px-10" 
-                            href={"./create"}
-                            id="create">
+                    <div className="flex flex-col justify-center space-y-12"> 
+                        <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
+                            <Link className = "text-xl font-bold tracking-widest px-10" 
+                                href={"./create"}
+                                id="create">
                               CREATE
-                      </Link>
+                            </Link>
                     
-                  </button>
-                  <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
-                    <Link className = "text-xl font-bold tracking-widest px-10"
-                    href={"./join"}
-                      id="create">
+                        </button>
+                        <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
+                            <Link className = "text-xl font-bold tracking-widest px-10"
+                                href={"./join"}
+                                id="create">
                       JOIN
-                    </Link>
+                            </Link>
                         
-                  </button>
+                        </button>
+                    </div>
                 </div>
             </div>
-         </div>
 
             
 
         
 
-    </main>
-  );
+        </main>
+
+    );
 }
