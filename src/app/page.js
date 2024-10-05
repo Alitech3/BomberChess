7975e629ff3020dbbaf29f93873d322f30e40f41
ckfrancois <francois.chelseak@gmail.com>
2024-10-05 13:11:18 -0400

    Update page.js
    Updated the UI + added create/join lobby buttons (can be replaced later)

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,35 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Link href={"./game"}>Game</Link>
+    <main className="flex flex-col items-center justify-start h-screen bg-gradient-to-tr from-green-700 to-green-400">
+
+        <div className = "flex justify-center items-center mt-10 mb-28">
+              
+          <h1 className="text-6xl font-bold tracking-wide my-12">
+              Bomber Chess
+          </h1>
+            
+        </div>
+          
+        <div className="flex flex-col justify-center space-y-12"> 
+          <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
+              <Link className = "text-xl font-bold tracking-widest px-10" 
+                    href={"./create"}
+                    id="create">
+                      CREATE
+              </Link>
+            
+          </button>
+          <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
+            <Link className = "text-xl font-bold tracking-widest px-10"
+            href={"./join"}
+              id="create">
+              JOIN
+            </Link>
+                
+          </button>
+        </div>
+
     </main>
   );
 }
