4bf6ec33d180cf86031dcb1ff544354cd6d9e084
ckfrancois <francois.chelseak@gmail.com>
2024-10-05 18:00:45 -0400

    pages

--- src/app/create/create.js
+++ src/app/create/create.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Create() {
   return (
     <main className="h-screen bg-center bg-gradient-to-tr from-green-700 to-green-400">
 
@@ -11,22 +11,20 @@ export default function Home() {
               <div className = "flex flex-col justify-center items-center mt-10 mb-28">
                   
                   <h1 className="text-6xl font-bold tracking-wide my-12">
-                    Bomber Chess
+                    create
                   </h1>  
                 </div>
                   
                 <div className="flex flex-col justify-center space-y-12"> 
-                  <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
-                        <Link className = "text-xl font-bold tracking-widest px-10" 
-                            href={"./create"}
-                            id="create">
-                            CREATE
-                        </Link>
-                  </button>
+                <div className>
+                        <input type="text" id="username" class="bg-yellow-500 border border-yellow-600 text-white rounded-full shadow-md text-xl font-bold tracking-widest px-10
+                            focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5" placeholder="Username" required />
+                    </div>
+
                   <button className="p-12 bg-yellow-500 text-white rounded-full shadow-md hover:bg-yellow-600 transition hover:text-black duration-200">
                         <Link className = "text-xl font-bold tracking-widest px-10"
                             href={"./join"}
-                            id="join">
+                            id="create">
                             JOIN
                         </Link>
                   </button>
