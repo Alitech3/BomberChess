6ed26f6d5b4dc12cd64939cbf55d865d55f539b5
ManAntGithub <jo638875@ucf.edu>
2024-10-06 02:17:20 -0400

    Fixed some styling

--- src/app/lobby/page.js
+++ src/app/lobby/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <main className="h-screen bg-center bg-gradient-to-tr from-green-700 to-green-400">
             <div className="flex flex-col items-center justify-start h-screen bg-repeat bg-[length:106px] bg-bomb-pattern">
                 <div className="flex flex-col justify-center items-center h-screen bg-green-800">
-                    <button id="button" className="px-8 py-5 bg-red-500 text-white rounded-full shadow-md hover:bg-red-600 transition hover:text-black duration-200
+                    <button id="button" className="px-8 py-5 bg-red-500 text-white rounded-full shadow-md
                                 outline-none bg-red-500 border-4 border-red-800 rounded-full text-6xl font-bold tracking-wide my-12 ">
                         UNIQUE ID</button>
                     <div id="outline" className="flex flex-col justify-center items-center border-none">
@@ -16,9 +16,9 @@ export default function Home() {
                             <div className="flex flex-col justify-center items-center mt-10 mb-28">
                                 {/* <div className="row"> */}
                                 <div className="column">
-                                    <button id="button" className="mx-8 px-8 py-5 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 transition hover:text-black duration-200
+                                    <button id="button" className="mx-8 px-8 py-5 bg-red-500 text-white rounded-lg shadow-md 
                                 outline-none bg-red-500 border-4 border-red-800 text-3xl font-bold"> Team 1 </button>
-                                    <button id="button" className="mx-8 px-8 py-5 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 transition hover:text-black duration-200
+                                    <button id="button" className="mx-8 px-8 py-5 bg-red-500 text-white rounded-lg shadow-md 
                                 outline-none bg-red-500 border-4 border-red-800 text-3xl font-bold"> Team 2 </button>
                                 </div>
                                 <div className="my-14 grid grid-cols-2 gap-4 justify-center items-center">
