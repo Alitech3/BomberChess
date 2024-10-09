84b72013eb6d41f095ca90ce6b964bbb21c8bff2
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-10-09 01:40:34 -0400

    linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+        "no-unused-vars": "error",
+        "no-undef": "error",
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-page-custom-font': 'off',
+        'indent': ['warn', 4],
+        'linebreak-style': ['warn', 'windows'],
+        'quotes': ['warn', 'double'],
+        'semi': ['warn', 'always'],
+    }
+}
 ];
