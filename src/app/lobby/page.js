e72690f658b1c3b04fe2b733f2e58c53ccc55855
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-10-09 07:30:10 -0400

    lobby logic

--- src/app/lobby/page.js
+++ src/app/lobby/page.js
@@ -1,47 +1,68 @@
 "use client";
 import Link from "next/link";
-import { useAtom } from "jotai";
-import { lobbyId } from "@/atom";
 import { useState, useEffect} from "react";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot, updateDoc, arrayUnion } from "firebase/firestore";
+import { useRouter } from "next/navigation.js";
+
 import { db } from "../../../firebase.js"; // Import Firestore instance from your firebase config
 
-export default function Home() {
-    const [Atomid] = useAtom(lobbyId);
-    const [id, setId] = useState("");
+export default function Home({searchParams}) {
+    const router = useRouter();
+    const [gameData, setGameData] = useState();
+    const [lobbyId] = useState(searchParams.id);
+    const [un] = useState(searchParams.un);
+    const [lobby, setLobby] = useState();
+    const [lobbyRef, setLobbyRef] = useState();
 
     useEffect(() => {
-        setId(Atomid);
-        console.log(id);
-    }, [Atomid, id]);
-    const queryLobbyById = async (lobbyId) => {
-        try {
-            // Reference to the lobby document by ID
+        (async ()=>{
             const lobbyRef = doc(db, "lobbies", lobbyId);
-          
-            // Fetch the document from Firestore
-            const docSnap = await getDoc(lobbyRef);
-          
-            if (docSnap.exists()) {
-            // If the document exists, return the data
-                console.log("Lobby data:", docSnap.data());
-                return docSnap.data();
-            } else {
-            // If the document does not exist, return null
-                console.log("No such lobby found");
-                return null;
-            }
+            setLobbyRef(lobbyRef);
+            // setLobby(await getDoc(lobbyRef));
+        })();
+
+        onSnapshot(doc(db, "lobbies", lobbyId), (doc) => {
+
+            const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+            // if (source === "Server") {
+            //     console.log(gameData);
+            // }
+            setGameData(doc.data());
+            console.log("Current data: ", doc.data());
+            console.log(source, " data: ", doc.data());
+        });
+    }, []);
+
+    // check if different user already occupies that slot
+    const joinTeam = async (un, board, color) => {
+        console.log(un + board + color);
+        try {
+            await updateDoc(lobbyRef, {
+                participants: arrayUnion(un),
+                [`${board}.${color}`]: un
+            });
+        }
+        catch(e) {
+            console.log(e);
+        }
+    };
+
+    const startGame = async () => {
+        try {
+            router.push(`/game?id=${lobbyId}&un=${un}`);
         } catch (error) {
             console.error("Error querying lobby:", error);
             return null;
         }
     };
+
     return (
         <main className="h-screen bg-center bg-gradient-to-tr from-green-700 to-green-400">
             <div className="flex flex-col items-center justify-start h-screen bg-repeat bg-[length:106px] bg-bomb-pattern">
                 <div className="flex flex-wrap h-screen items-center justify-center w-1/2 bg-green-600 rounded-lg m-0 p-0">
-                    <button className="flex flex-wrap h-fit bg-[#56b0ce] text-white rounded-3xl outline-none text-4xl font-bold tracking-wide p-2 px-2 border-1 border-black border-2">
-            Lobby Id:<br/> {id}
+                    <button className="flex flex-wrap h-fit bg-[#56b0ce] text-white rounded-3xl outline-none text-4xl font-bold tracking-wide p-2 px-2 border-1 border-black border-2 select-text">
+                        Lobby Id:<br/>
+                        {lobbyId}
                     </button>
                     <div className="flex flex-col w-full">
                         <div className="flex justify-around w-full">
@@ -49,19 +70,24 @@ export default function Home() {
                             <button className="ml-4 px-3 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200 w-1/3">  Team 2 </button>
                         </div>
                         <div className="flex justify-evenly items-center ">
-                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"> 
-                                Player 1 </button>
-                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200">  Player 2 </button>
-                            <button className="px-3 py-1 bg-orange-600 text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200">  Rotate </button>
-                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"> Player 3</button>
-                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200">Player 4 </button>
+                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"  onClick={() => joinTeam(un, "board1", "white")}>
+                                {(gameData) ? gameData.board1.white : "white"}
+                            </button>
+                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"onClick={() => joinTeam(un, "board2", "black")}>
+                                {(gameData) ? gameData.board2.black : "black"}
+                            </button>
+                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"onClick={() => joinTeam(un, "board2", "white")}>
+                                {(gameData) ? gameData.board2.white : "white"}
+                            </button>
+                            <button className="m-3 px-2 py-1 bg-[#56b0ce] text-white shadow-md border-black border-2 text-3xl font-bold rounded-3xl hover:bg-red-600 transition hover:text-black duration-200"onClick={() => joinTeam(un, "board1", "black")}>
+                                {(gameData) ? gameData.board1.black : "black"}
+                            </button>
                         </div>
-                        <button className="px-2 py-3 bg-red-500 text-white rounded-full shadow-md hover:bg-red-600 transition hover:text-black duration-200
-        outline-none bg-red-500 border-4 border-red-800 rounded-full text-3xl font-bold col-span-3" onClick={()=>{queryLobbyById();}}>
+                        <button className="px-2 py-3 bg-red-500 text-white rounded-full shadow-md hover:bg-red-600 transition hover:text-black duration-200 outline-none bg-red-500 border-4 border-red-800 rounded-full text-3xl font-bold col-span-3" onClick={()=>{startGame();}}>
                             <Link className = "text-xl font-bold tracking-widest w-2 h-5" 
-                                href={"/game"}
+                                href={""}
                                 id="game">
-        START
+                                START
                             </Link></button>
                     </div>
                 </div >
