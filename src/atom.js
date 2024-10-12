7caaf32a6bb0b9a09facf5f11b12fab11529580c
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-10-12 06:38:49 -0400

    state manager
    i hate state management i just want to pass this info via url why must i make it so complicated

--- src/atom.js
+++ src/atom.js
@@ -1,3 +1,4 @@
 import { atom } from "jotai";
-const lobbyId = atom(null);
-export { lobbyId };
+const Data = atom(undefined);
+const Active = atom({user: undefined, board: undefined, lobbyId: undefined});
+export { Data, Active };
